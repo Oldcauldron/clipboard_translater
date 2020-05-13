101a22916d3955cd08c10daca5c7336b7b477af0
sapereaude <atiacht@gmail.com>
2020-05-13 11:38:30 +0300

    new feautures

--- translate.py
+++ translate.py
@@ -6,9 +6,9 @@ import pyautogui
 import subprocess
 import time
 
-translator = Translator()
 
 pyperclip.copy('')
+
 past = ''
 
 
@@ -18,40 +18,51 @@ def conn(past):
     return result, translator
 
 
-while True:
+if __name__ == '__main__':
 
-    first = datetime.datetime.now()
+    pyautogui.alert(f'Copy [stop] for exit, or [pause] for pause')
 
-    while not past:
-        now = datetime.datetime.now().minute - first.minute
-        if now == 1:
-            _, translator = conn('one')
-            first = datetime.datetime.now()
-        time.sleep(1)
-        past = pyperclip.paste()
+    _, translator = conn('one')
 
-    if past == 'stop':
-        pyautogui.alert(f'Exit')
-        break
+    while True:
 
-    if type(past) == str:
-        past = past.replace('\n', ' ').replace('\r', '').lower()
-        try:
-            result = translator.translate(past, src='en', dest='ru')
-        except Exception as err:
-            logger.info(f'EXCEPTION - {err}')
-        finally:
-            result, translator = conn(past)
-        message = pyautogui.confirm(text=f'{past} - {result.text}',
-                                    title='writing in file',
-                                    buttons=["write it", "don't write"])
-        if message == 'write it':
-            with open('new_dict.txt', 'a') as nd:
-                writeit = f'{past}, {result.text.lower()}\n'
-                nd.write(writeit)
-                print(writeit)
-    past = pyperclip.copy('')
+        first_time = datetime.datetime.now()
 
-subprocess.Popen(['start', 'new_dict.txt'], shell=True)
+        while not past:
+            now = datetime.datetime.now().minute - first_time.minute
+            if now >= 1:
+                _, translator = conn('one')
+                first_time = datetime.datetime.now()
+            time.sleep(1)
+            past = pyperclip.paste()
 
+        if past == 'pause':
+            past = pyautogui.confirm(
+                text='PAUSE! Please sczmack continue or stop and exit',
+                title='helper', buttons=['continue', 'stop'])
+            if past == 'continue':
+                past = pyperclip.copy('')
 
+        if past == 'stop':
+            pyautogui.alert(f'Exit')
+            break
+
+        if type(past) == str:
+            past = past.replace('\n', ' ').replace('\r', '').lower()
+            try:
+                result = translator.translate(past, src='en', dest='ru')
+            except Exception as err:
+                logger.info(f'EXCEPTION - {err}')
+            finally:
+                result, translator = conn(past)
+            message = pyautogui.confirm(text=f'{past} - {result.text}',
+                                        title='writing in file',
+                                        buttons=["write it", "don't write"])
+            if message == 'write it':
+                with open('new_dict.txt', 'a') as nd:
+                    writeit = f'{past}, {result.text.lower()}\n'
+                    nd.write(writeit)
+                    print(writeit)
+        past = pyperclip.copy('')
+
+    subprocess.Popen(['start', 'new_dict.txt'], shell=True)
